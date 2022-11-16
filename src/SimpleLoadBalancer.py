acc45f7934c430e7d8127f2c89f704083458dbb8
root <root@calpino.localdomain>
2022-11-16 17:38:39 +0200

    updated SimpleLoadBalancer.py shcript

--- src/SimpleLoadBalancer.py
+++ src/SimpleLoadBalancer.py
@@ -4,6 +4,7 @@ import pox.openflow.libopenflow_01 as of
 from pox.lib.packet.arp import arp
 from pox.lib.packet.ipv4 import ipv4
 from pox.lib.addresses import EthAddr, IPAddr
+from pox.lib.packet.ethernet import ethernet, ETHER_BROADCAST
 log = core.getLogger()
 import time
 import random
@@ -19,6 +20,10 @@ class SimpleLoadBalancer(object):
         
         # add the necessary openflow listeners
         core.openflow.addListeners(self) 
+        
+        # our table for storing mac,port when servers reply to ARP req
+        self.mac_ports = {}
+        self.mac_portc= {}
 
         # set class parameters
         self.lb_mac=lb_mac
@@ -43,14 +48,28 @@ class SimpleLoadBalancer(object):
 
     # send ARP reply "proxied" by the controller (on behalf of another machine in network)
     def send_proxied_arp_reply(self, packet, connection, outport, requested_mac):
-        # write your code here!!!
-        pass
+        arp_reply = arp()
+        arp_reply.hwsrc = requested_mac
+        arp_reply.hwdst = packet.src
+        arp_reply.opcode = arp.REPLY
+        arp_reply.protosrc = packet.next.protodst
+        arp_reply.protodst = packet.payload.protosrc
+        ether = ethernet()
+        ether.type = ethernet.ARP_TYPE
+        ether.dst = packet.src
+        ether.src = requested_mac
+        ether.payload = arp_reply
+        msg = of.ofp_packet_out()
+        msg.data=ether.pack()
+        msg.actions.append(of.ofp_action_output(port = outport))
+        connection.send(msg)
+        
 
 
     # send ARP request "proxied" by the controller (so that the controller learns about another machine in network)
     def send_proxied_arp_request(self, connection, ip):
     #send ARP req to find servers mac addr
-        arp_req = arp_serv()
+        arp_req = arp()
         arp_req.hwtype = arp_req.HW_TYPE_ETHERNET
         arp_req.prototype = arp_req.PROTO_TYPE_IP
         arp_req.hwlen = 6
@@ -58,27 +77,49 @@ class SimpleLoadBalancer(object):
         arp_req.opcode = arp_req.REQUEST
         arp_req.hwdst = ETHER_BROADCAST
         arp_req.protodst = IPAddr(ip)
-        arp_req.hwsrc = EthAddr("0A:00:00:00:00:01")
-        arp_req.protosrc = IPAddr("10.1.2.3")
-        e = ethernet(type=ethernet.ARP_TYPE, src=EthAddr("0A:00:00:00:00:01") , dst=ETHER_BROADCAST)
+        arp_req.hwsrc = EthAddr("0A:00:00:00:00:01") #src fake mac of switch
+        arp_req.protosrc = IPAddr("10.1.2.3")#src fake ip of service
+        e = ethernet(type=ethernet.ARP_TYPE, src=EthAddr("0A:00:00:00:00:01") , dst=ETHER_BROADCAST) #broadcast to find everyone
         e.set_payload(arp_req)
         msg = of.ofp_packet_out()
-        msg.data = e.pack() #peossssssss aaaaaaa
-        msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+        msg.data = e.pack()
+        msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))#flood to find everyone
         connection.send(msg)
         print("LB::Sended ARP reqs to servers.")
 
     
     # install flow rule from a certain client to a certain server
     def install_flow_rule_client_to_server(self, connection, outport, client_ip, server_ip, buffer_id=of.NO_BUFFER):
-        # write your code here!!!
-        pass
+        msg = of.ofp_flow_mod()
+        msg.priority = 42
+        msg.match.dl_type = 0x800
+        msg.match.nw_dst = IPAddr(self.service_ip) #match if dst is service ip
+        msg.match.nw_src = client_ip #and if src is client ip
+        msg.idle_timeout=15 #15 secs are fair enough i guess
+        #now if above matches actions are; set ipdst=server_ip, macsrc=lb mac, macdst=servermac and port = outport 
+        msg.actions.append(of.ofp_action_nw_addr.set_dst(server_ip))
+        msg.actions.append(of.ofp_action_dl_addr.set_src(EthAddr(self.lb_mac)))
+        msg.actions.append(of.ofp_action_dl_addr.set_dst(EthAddr(self.mac_ports[server_ip][0])))
+        msg.actions.append(of.ofp_action_output(port = outport))
+
+        self.connection.send(msg)
 
 
     # install flow rule from a certain server to a certain client
     def install_flow_rule_server_to_client(self, connection, outport, server_ip, client_ip, buffer_id=of.NO_BUFFER):
-        # write your code here!!!
-        pass
+        msg = of.ofp_flow_mod()
+        msg.priority = 42
+        msg.match.dl_type = 0x800
+        msg.match.nw_dst = IPAddr(client_ip) #match if dst is client ip
+        msg.match.nw_src = server_ip #and if src is server ip
+        msg.idle_timeout=15 #15 secs are fair enough i guess
+        #now if above matches actions are: set ipdst=client_ip, macsrc=lb mac, macdst=clientmac and port = outport 
+        msg.actions.append(of.ofp_action_nw_addr.set_dst(client_ip))
+        msg.actions.append(of.ofp_action_dl_addr.set_src(EthAddr(self.lb_mac)))
+        msg.actions.append(of.ofp_action_dl_addr.set_dst(EthAddr(self.mac_portc[client_ip][0])))
+        msg.actions.append(of.ofp_action_output(port = outport))
+
+        self.connection.send(msg)
 
 
     # main packet-in handling routine
@@ -86,29 +127,96 @@ class SimpleLoadBalancer(object):
         packet = event.parsed
         connection = event.connection
         inport = event.port
-
         if packet.type == packet.ARP_TYPE:
             if packet.payload.opcode == arp.REQUEST:
-                """arp_reply = arp()
-                arp_reply.hwsrc = <requested mac address>
-                arp_reply.hwdst = packet.src
-                arp_reply.opcode = arp.REPLY
-                arp_reply.protosrc = <IP of requested mac-associated machine>
-                arp_reply.protodst = packet.payload.protosrc
-                ether = ethernet()
-                ether.type = ethernet.ARP_TYPE
-                ether.dst = packet.src
-                ether.src = <requested mac address>
-                ether.payload = arp_reply"""
-                #send this packet to the switch
-                #see section below on this topic
+                if packet.next.protosrc in self.server_ips and packet.next.protodst in self.mac_portc:
+                    print ("LB::Detected server ARP request. ")+str(packet.next.protosrc)
+                    self.send_proxied_arp_reply(packet, connection, inport, self.lb_mac)
+                    print ("LB::Sended ARP reply to server in port. ")+str(inport)
+                elif packet.next.protosrc not in self.server_ips and packet.next.protodst == self.service_ip:
+                    print ("LB::Detected client ARP request. ")+str(packet.next.protosrc)
+                    #adding to key(ip address of client) of table the coresponding mac,port
+                    self.mac_portc[packet.next.protosrc] = (packet.src,inport)
+                    print ("LB::Current mac_portc table:")
+                    for key,value in self.mac_portc.items():
+                        print(key,value)
+                    self.send_proxied_arp_reply(packet, connection, inport, self.lb_mac)
+                    print ("LB::Sended ARP reply to client in port. ")+str(inport)
+                    #send this packet to the switch
+                    #see section below on this topic
+                else:
+                    print ("LB::Detected false ARP request |OR| ARP destination not in ip,mac,port mapings.")
             elif packet.payload.opcode == arp.REPLY:
                 print ("LB::Server replied. ") + str(packet.next.protosrc) 
+                print ("LB::Adding server mac-port to mac_table.")
+                #adding to key(ip address of server) of table the coresponding mac,port
+                self.mac_ports[packet.next.protosrc] = (packet.src,inport)
+                print ("LB::Current mac_ports table:")
+                for key,value in self.mac_ports.items():
+                    print(key,value)
             else:
-                print "Some other ARP opcode, probably do something smart here" 
+                print ("LB::Error detected wrong ARP opcode.")
         elif packet.type == packet.IP_TYPE:
-            # write your code here!!!
-            pass
+            if packet.payload.srcip in self.mac_portc and packet.payload.dstip==self.service_ip:
+                print ("LB::Client IP packet without flow rule arrived installing a flow table entry for ClientToServer.")
+                #hardcoded make flow rule and send packet to coresponding server by color
+                if self.user_ip_to_group[packet.payload.srcip]== "red":
+                    if random.randint(0, 1):
+                        self.install_flow_rule_client_to_server(connection,5,packet.payload.srcip,IPAddr("10.0.0.5"))
+                        packet.src=self.lb_mac
+                        packet.payload.dstip=IPAddr("10.0.0.5")
+                        packet.dst=self.mac_ports[IPAddr("10.0.0.5")][0]
+
+                        icmp_req_packet=of.ofp_packet_out()
+                        icmp_req_packet.data=packet
+                        icmp_req_packet.actions.append(of.ofp_action_output(port=5))
+                        connection.send(icmp_req_packet)
+                    else:
+                        self.install_flow_rule_client_to_server(connection,6,packet.payload.srcip,IPAddr("10.0.0.6"))
+                        packet.src=self.lb_mac
+                        packet.payload.dstip=IPAddr("10.0.0.6")
+                        packet.dst=self.mac_ports[IPAddr("10.0.0.6")][0]
+
+                        icmp_req_packet=of.ofp_packet_out()
+                        icmp_req_packet.data=packet
+                        icmp_req_packet.actions.append(of.ofp_action_output(port=6))
+                        connection.send(icmp_req_packet)
+                else:
+                    if random.randint(0, 1):
+                        self.install_flow_rule_client_to_server(connection,7,packet.payload.srcip,IPAddr("10.0.0.7"))
+                        packet.src=self.lb_mac
+                        packet.payload.dstip=IPAddr("10.0.0.7")
+                        packet.dst=self.mac_ports[IPAddr("10.0.0.7")][0]
+
+                        icmp_req_packet=of.ofp_packet_out()
+                        icmp_req_packet.data=packet
+                        icmp_req_packet.actions.append(of.ofp_action_output(port=7))
+                        connection.send(icmp_req_packet)
+                    else:
+                        self.install_flow_rule_client_to_server(connection,8,packet.payload.srcip,IPAddr("10.0.0.8"))
+                        packet.src=self.lb_mac
+                        packet.payload.dstip=IPAddr("10.0.0.8")
+                        packet.dst=self.mac_ports[IPAddr("10.0.0.8")][0]
+
+                        icmp_req_packet=of.ofp_packet_out()
+                        icmp_req_packet.data=packet
+                        icmp_req_packet.actions.append(of.ofp_action_output(port=8))
+                        connection.send(icmp_req_packet)
+            elif packet.payload.srcip in self.mac_ports and packet.payload.dstip in self.mac_portc :
+                #make flow rule and send packet to coresponding client by looking at packet 
+                print ("LB::Server IP packet without flow rule arrived installing a flow table entry for ServerToClient.")
+                self.install_flow_rule_server_to_client(connection,self.mac_portc[packet.payload.dstip][1],packet.payload.srcip,packet.payload.dstip)
+                packet.src=self.lb_mac
+                packet.payload.srcip=self.service_ip
+                packet.dst=self.mac_portc[packet.payload.dstip][0]
+                
+                icmp_rep_packet=of.ofp_packet_out()
+                icmp_rep_packet.data=packet
+                icmp_rep_packet.actions.append(of.ofp_action_output(port=self.mac_portc[packet.payload.dstip][1]))
+                connection.send(icmp_rep_packet)
+            else:
+                print ("LB::Error packet src IP not in <IP,MAC,PORT> tables.")
+
         else:
             log.info("Unknown Packet type: %s" % packet.type)
             return
